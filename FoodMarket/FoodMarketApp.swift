5927a9a59f82061dfb61735869a692cdc1c525e1
keshew <artyom.korotckov2017@yandex.ru>
2025-07-04 18:45:50 +0500

    Initial Commit

---
+++ FoodMarket/FoodMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMarketApp.swift
+//  FoodMarket
+//
+//  Created by Артём Коротков on 04.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
